ff3277c991799cd16f093302d99ddd88808f8f6f
vantrung2003 <tranvantrung2003tb@gmail.com>
2023-03-30 09:57:05 +0700

    datban

--- app/src/main/java/com/example/fastfood/BanActivity.java
+++ app/src/main/java/com/example/fastfood/BanActivity.java
@@ -18,7 +18,7 @@ public class BanActivity extends AppCompatActivity {
 
     GridView gridView;
     TableDao tableDao;
-
+ // Đặt bàn actuvity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
